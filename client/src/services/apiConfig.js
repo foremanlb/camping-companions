37b3781781c03a15584c1ae94a8402a84680bf20
Craig Navarro <craignavarro@Craigs-MacBook-Pro.local>
2021-05-10 07:46:42 -0700

    apiConfig service done

---
+++ client/src/services/apiConfig.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+//token from local storage
+const getToken = async () => {
+  let token = (await localStorage.getItem("token")) || null;
+  return `Bearer ${token}`;
+};
+
+let apiUrl;
+
+const apiUrls = {
+  production: "",
+  development: "http://localhost:3000/",
+};
+
+if (window.location.hostname === "localhost") {
+  apiUrl = apiUrls.development;
+} else {
+  apiUrl = apiUrls.production;
+}
+
+const api = axios.create({
+  baseURL: apiUrl,
+});
+
+api.interceptors.request.use(
+  async (options) => {
+    options.headers["Authorization"] = await getToken();
+    return options;
+  },
+  (error) => {
+    console.log("Request Error:", error);
+    return Promise.reject(error);
+  }
+);
+
+export default api;
