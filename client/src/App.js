70b61a2ca3c7c9c448ba67b598a86e2b9db31d56
Craig Navarro <craignavarro@Craigs-MacBook-Pro.local>
2021-05-11 14:10:33 -0700

    login done

--- client/src/App.js
+++ client/src/App.js
@@ -8,17 +8,21 @@ import Campsites from './screens/Campsites/Campsites'
 import { getCampsites } from './services/campsites'
 import CampsiteDetails from './screens/CampsiteDetails/CampsiteDetails'
 import Post from './screens/Post/Post'
-import {getPosts} from './services/posts'
+import { getPosts } from './services/posts'
+import { verifyUser } from './services/auth'
+import Login from './components/Login/Login'
 
 
 function App() {
   const [isActive, setIsActive] = useState(false)
   const [campsites, setCampsites] = useState([])
   const [posts, setPosts] = useState([])
+  const [currentUser, setCurrentUser] = useState(null)
 
   useEffect(() => {
     fetchCampsites()
     fetchPosts()
+    verify()
   }, [])
   
   const fetchPosts = async () => {
@@ -31,12 +35,18 @@ function App() {
     setCampsites(data)
   }
 
+  const verify = async () => {
+    let user = await verifyUser()
+    setCurrentUser(user)
+  }
+
   return (
     <div className="App">
       <Header />
       <div className='navbar-container'>
         <Navbar isActive={isActive} setIsActive={setIsActive}/>
         <div className={isActive ? 'main-content' : 'main-content_large'}>
+          <Login currentUser={currentUser} verify={verify}/>
           <Route exact path='/'>
             <Home />
           </Route>
---
+++ client/src/components/Login/Login.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import { loginUser } from '../../services/auth'
+import {Link} from 'react-router-dom'
+
+export default function Login(props) {
+  const [input, setInput] = useState({})
+  
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setInput((prevState) => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await loginUser(input)
+    props.verify()
+  }
+
+  const renderLoginBar = () => {
+    if (!props.currentUser) {
+      return (
+        <div>
+          <form onChange={handleChange} onSubmit={handleSubmit}>
+            <label>Email</label>
+            <input name='email' type='email' />
+            <label>Password</label>
+            <input name='password' type='password' />
+            <button type='submit'>Log In</button>
+          </form>
+          <Link to='/signup'>Sign Up</Link>
+        </div>
+      )
+    } 
+  }
+  
+  return (
+    <div>
+      {renderLoginBar()}
+    </div>
+  )
+}
