3a50d5071e159b0b665ed25054d0dcbb73aea0b2
Craig Navarro <craignavarro@Craigs-MacBook-Pro.local>
2021-05-12 13:22:51 -0700

    header css

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
       </Link>
       <h1 id='title'>Camping Companions</h1>
       <Link to='/profile'>
-        <h1 id='profile_link'>X</h1>
+        <img url='https://raw.githubusercontent.com/foremanlb/camping-companions/33893b7356b6d64d586228893118b445aec4e3b3/client/src/assets/images/home_icon.svg' alt='Home Icon'></img>
       </Link>
     </div>
   )
