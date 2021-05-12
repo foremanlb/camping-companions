33893b7356b6d64d586228893118b445aec4e3b3
Craig Navarro <craignavarro@Craigs-MacBook-Pro.local>
2021-05-12 13:20:00 -0700

    header css

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
       <Link to='/'>
         <h1 id='home_link'>X</h1>
       </Link>
-      <h1>Camping Companions</h1>
+      <h1 id='title'>Camping Companions</h1>
       <Link to='/profile'>
         <h1 id='profile_link'>X</h1>
       </Link>
